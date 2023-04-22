506f25b4e93c00d3e0e41727654472bcc7cb605c
Felipe <pipeba1408@gmail.com>
2023-04-22 13:28:00 -0500

    firebase.js

--- src/App.js
+++ src/App.js
@@ -21,9 +21,6 @@ function App() {
                 <Route path = '/BlogPost' element={<BlogPost/>}/>
             </Routes>
         </React.Fragment>
-
-        
-
     )
     
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCGOboTnORoCaSE2EacYcQLjw0k5jba0IQ",
+    authDomain: "reactblog-c066c.firebaseapp.com",
+    projectId: "reactblog-c066c",
+    storageBucket: "reactblog-c066c.appspot.com",
+    messagingSenderId: "1067720582815",
+    appId: "1:1067720582815:web:7d2281c0a85657d1d624aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
